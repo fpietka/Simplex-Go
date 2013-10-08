574f658e3f11f89f8d59387bb2244db02d80db2a
François Pietka <francois@pietka.fr>
2013-10-08 15:15:15 +0200

    fix indentation

--- simplexnoise.go
+++ simplexnoise.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "math"
-    "math/rand"
-    "os"
-    "encoding/binary"
+	"fmt"
+	"time"
+	"math"
+	"math/rand"
+	"os"
+	"encoding/binary"
 )
 
 const (
-    hgrid int = 500 // x dimension of the grid
-    vgrid int = 500 // y dimension of the grid
+	hgrid int = 500 // x dimension of the grid
+	vgrid int = 500 // y dimension of the grid
 )
 
 type color struct {
 	//r=red, g=green, b=blue
-    v[3] int
+	v[3] int
 }
 
 
 func main() {
-    beginning := time.Now()
+	beginning := time.Now()
 
-    //set the random seed
+	//set the random seed
 	rand.Seed(beginning.UnixNano())
 
-    //make the empty array
+	//make the empty array
 	var grid[hgrid][vgrid] float64
 
 	var min, max float64
@@ -37,19 +37,19 @@ func main() {
 	//now that we have an interesting map, create a .BMP file
 	printMap(grid, min, max)
 
-    end := time.Now()
-    printPage(beginning, end)
+	end := time.Now()
+	printPage(beginning, end)
 }
 
 func lerp(c1, c2 color, value float64) color {
-    var tcolor = color{[3]int{0, 0, 0}}
+	var tcolor = color{[3]int{0, 0, 0}}
 
 	for g := 0; g < 3; g++ {
 		if c1.v[g] > c2.v[g] {
-            tcolor.v[g] = c2.v[g] + int((float64(c1.v[g] - c2.v[g]) * value))
-        } else {
-            tcolor.v[g] = c1.v[g] + int((float64(c2.v[g] - c1.v[g]) * value))
-        }
+			tcolor.v[g] = c2.v[g] + int((float64(c1.v[g] - c2.v[g]) * value))
+		} else {
+			tcolor.v[g] = c1.v[g] + int((float64(c2.v[g] - c1.v[g]) * value))
+		}
 	}
 
 	return tcolor
@@ -61,10 +61,10 @@ func fillMap(grid[hgrid][vgrid] float64, min float64, max float64) ([hgrid][vgri
 	var octaves int = 16
 
 	//these variables are used by the fBm part, but not extensively in the noise part
-    var pixel_value, amplitude, frequency, gain, lacunarity float64
-    //physics terms. gain affects the amplitude each octave, lacunarity affects the frequency
-    gain = 0.65
-    lacunarity = 2.0
+	var pixel_value, amplitude, frequency, gain, lacunarity float64
+	//physics terms. gain affects the amplitude each octave, lacunarity affects the frequency
+	gain = 0.65
+	lacunarity = 2.0
 
 	//these are all variables used only by the noise function, not the fBm part
 	var disbx, disby float64 //distances to the three corners
@@ -95,7 +95,7 @@ func fillMap(grid[hgrid][vgrid] float64, min float64, max float64) ([hgrid][vgri
 	var permutations[256] int //make it as long as the largest dimension
 	for i = 0; i < 256; i++ {
 		permutations[i] = i //put each number in once
-    }
+	}
 
 	//randomize the random numbers table
 	for i = 0; i < 256; i++  {
@@ -155,21 +155,21 @@ func fillMap(grid[hgrid][vgrid] float64, min float64, max float64) ([hgrid][vgri
 				gradt = permutations[(cornertx + permutations[cornerty & 255]) & 255] & 7
 
 				//get the noise from each corner using an attenuation function
-				//	first the bottom corner
+				//first the bottom corner
 				tempdis = 0.5 - disbx * disbx - disby * disby
 				if tempdis < 0.0 {
 					noiseb = 0.0
 				} else {
 					noiseb = math.Pow(tempdis, 4.0) * dotproduct(gradients[gradb], disbx, disby)
-                }
+				}
 
-				//	then the middle corner
+				//then the middle corner
 				tempdis = 0.5 - dismx * dismx - dismy * dismy
 				if tempdis < 0.0 {
 					noisem = 0.0
-                } else {
+				} else {
 					noisem = math.Pow(tempdis, 4.0) * dotproduct(gradients[gradm], dismx, dismy)
-                }
+				}
 
 				//	last the top corner
 				tempdis = 0.5 - distx * distx - disty * disty
@@ -177,7 +177,7 @@ func fillMap(grid[hgrid][vgrid] float64, min float64, max float64) ([hgrid][vgri
 					noiset = 0.0
 				} else {
 					noiset = math.Pow(tempdis, 4.0) * dotproduct(gradients[gradt], distx, disty)
-                }
+				}
 
 				//finally, add it in and adjust for the next layer
 				//	notice that no interpolation is needed, just straight summation
@@ -193,17 +193,17 @@ func fillMap(grid[hgrid][vgrid] float64, min float64, max float64) ([hgrid][vgri
 			//do some quick checks
 			if pixel_value < min {
 				min = pixel_value
-            } else if pixel_value > max {
+			} else if pixel_value > max {
 				max = pixel_value
-            }
+			}
 		}
 	}
 
-    return grid, min, max
+	return grid, min, max
 }
 
 func printMap(grid[hgrid][vgrid] float64, min float64, max float64) {
-    //set up some variables
+	//set up some variables
 	var diff float64 = max - min
 	var flood float64 = 0.5 //flood level
 	var mount float64 = 0.85 //mountain level
@@ -221,80 +221,80 @@ func printMap(grid[hgrid][vgrid] float64, min float64, max float64) {
 	var mountlow = color{[3]int{147, 157, 167}}
 	var mounthigh = color{[3]int{226, 223, 216}}
 
-    //3.0 output to file
+	//3.0 output to file
 	//3.1 Begin the file
 	//3.1.1 open output file
-    out, err := os.Create("test.bmp")
-    if err != nil {
-        fmt.Printf("Target file opening error\n")
-        panic(err)
-    }
+	out, err := os.Create("test.bmp")
+	if err != nil {
+		fmt.Printf("Target file opening error\n")
+		panic(err)
+	}
 
 	//3.1.2 copy the header
-    //3.1.2.1 magic number
-    out.Write(char(66))
-    out.Write(char(77))
+	//3.1.2.1 magic number
+	out.Write(char(66))
+	out.Write(char(77))
 
-    //3.1.2.2 filsize/unused space
-    for i = 0; i < 8; i++ {
-        out.Write(char(0))
-    }
+	//3.1.2.2 filsize/unused space
+	for i = 0; i < 8; i++ {
+		out.Write(char(0))
+	}
 
-    //3.1.2.3 data offset
-    out.Write(char(54))
+	//3.1.2.3 data offset
+	out.Write(char(54))
 
-    //3.1.2.4 unused space
-    for i = 0; i < 3; i++ {
-        out.Write(char(0))
-    }
+	//3.1.2.4 unused space
+	for i = 0; i < 3; i++ {
+		out.Write(char(0))
+	}
 
-    //3.1.2.5 header size
-    out.Write(char(40))
+	//3.1.2.5 header size
+	out.Write(char(40))
 
-    //3.1.2.6 unused space
-    for i = 0; i < 3; i++ {
-        out.Write(char(0))
-    }
+	//3.1.2.6 unused space
+	for i = 0; i < 3; i++ {
+		out.Write(char(0))
+	}
 
-    //3.1.2.7 file width (trickier)
-    out.Write(char(hgrid % 256))
-    out.Write(char((hgrid >> 8) % 256))
-    out.Write(char((hgrid >> 16) % 256))
-    out.Write(char((hgrid >> 24) % 256))
+	//3.1.2.7 file width (trickier)
+	out.Write(char(hgrid % 256))
+	out.Write(char((hgrid >> 8) % 256))
+	out.Write(char((hgrid >> 16) % 256))
+	out.Write(char((hgrid >> 24) % 256))
 
-    //3.1.2.8 file height (trickier)
-    out.Write(char(vgrid % 256))
-    out.Write(char((vgrid >> 8) % 256))
-    out.Write(char((vgrid >> 16) % 256))
-    out.Write(char((vgrid >> 24) % 256))
+	//3.1.2.8 file height (trickier)
+	out.Write(char(vgrid % 256))
+	out.Write(char((vgrid >> 8) % 256))
+	out.Write(char((vgrid >> 16) % 256))
+	out.Write(char((vgrid >> 24) % 256))
 
-    //3.1.2.9 color planes
-    out.Write(char(1))
-    out.Write(char(0))
+	//3.1.2.9 color planes
+	out.Write(char(1))
+	out.Write(char(0))
 
-    //3.1.2.10 bit depth
-    out.Write(char(24))
+	//3.1.2.10 bit depth
+	out.Write(char(24))
 
-    //3.1.2.11 the rest
-    for i = 0; i < 25; i++ {
-        out.Write(char(0))
-    }
+	//3.1.2.11 the rest
+	for i = 0; i < 25; i++ {
+		out.Write(char(0))
+	}
 
 	//3.2 put in the elements of the array
-    var newcolor = color{[3]int{0, 0, 0}}
+	var newcolor = color{[3]int{0, 0, 0}}
 	for i = (vgrid - 1); i >= 0; i-- { //bitmaps start with the bottom row, and work their way up...
 		for j = 0; j < hgrid; j++ { //...but still go left to right
 			grid[j][i] -= min
 			if grid[j][i] < flood {
-                //if this point is below the floodline...
+				//if this point is below the floodline...
 				newcolor = lerp(waterlow, waterhigh, grid[j][i] / flood)
 			} else if grid[j][i] > mount {
-                //if this is above the mountain line...
+				//if this is above the mountain line...
 				newcolor = lerp(mountlow, mounthigh, (grid[j][i] - mount) / (diff - mount))
-            } else {
-                //if this is regular land
+			} else {
+				//if this is regular land
 				newcolor = lerp(landlow, landhigh, (grid[j][i] - flood) / (mount - flood))
-            }
+			}
 
 			out.Write(char(newcolor.v[0])) //blue
 			out.Write(char(newcolor.v[1])) //green
@@ -303,7 +303,7 @@ func printMap(grid[hgrid][vgrid] float64, min float64, max float64) {
 		//round off the row
 		for k = 0; k < (hgrid % 4); k++ {
 			out.Write(char(0))
-        }
+		}
 	}
 
 	//3.3 end the file
@@ -311,26 +311,26 @@ func printMap(grid[hgrid][vgrid] float64, min float64, max float64) {
 }
 
 func char(value int) ([]byte) {
-    buf := make([]byte, binary.MaxVarintLen64)
-    binary.PutUvarint(buf, uint64(value))
+	buf := make([]byte, binary.MaxVarintLen64)
+	binary.PutUvarint(buf, uint64(value))
 
-    final := make([]byte, 1)
-    final[0] = buf[0]
-    return final
+	final := make([]byte, 1)
+	final[0] = buf[0]
+	return final
 }
 
 func random(max float64) float64 {
 	var r int
-    var s float64
+	var s float64
 
 	r = rand.Int()
-    s = float64(r & 0x7fff) / float64(0x7fff)
+	s = float64(r & 0x7fff) / float64(0x7fff)
 
-    return (s * max)
+	return (s * max)
 }
 
 func printPage(beginning, end time.Time) {
-    duration := end.Sub(beginning)
+	duration := end.Sub(beginning)
 	fmt.Printf("Content-Type: text/html\n\n")
 	fmt.Printf("<html><head><title>FTG Page</title></head>\n")
 	fmt.Printf("<body>\n")
@@ -343,11 +343,11 @@ func printPage(beginning, end time.Time) {
 
 func myfloor(value float64) (result int) {
 	if value >= 0 {
-        result = int(value)
-    } else {
-        result = int(value) - 1
-    }
-    return
+		result = int(value)
+	} else {
+		result = int(value) - 1
+	}
+	return
 }
 
 func dotproduct(grad [2]float64, x, y float64) float64 {
